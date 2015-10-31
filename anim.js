ca057389f36fc49452b9d330aebaeabd49dd5b30
Victor Tang <victortang1992@gmail.com>
2015-10-31 01:33:23 -0700

    add flowtype, min widths

---
+++ anim.js
@@ -0,0 +1,58 @@
+(function() {
+/*
+ * Starts any clocks using the user's local time
+ * From: cssanimation.rocks/clocks
+ */
+function initKnife() {
+  // Get the local time using JS
+  // Create an object with each hand and it's angle in degrees
+  var hands = [
+    {
+      hand: 'cyan',
+      angle: -45
+    },
+    {
+      hand: 'purp',
+      angle: -120
+    },
+    {
+      hand: 'grn',
+      angle: -202
+    },
+    {
+      hand: 'yllw',
+      angle: -282.8
+    }
+  ];
+  // Loop through each of these hands to set their angle
+  for (var j = 0; j < hands.length; j++) {
+    var elements = document.querySelectorAll('.' + hands[j].hand);
+    for (var k = 0; k < elements.length; k++) {
+        elements[k].style.webkitTransform = 'rotateZ('+ hands[j].angle +'deg)';
+        elements[k].style.MozTransform = 'rotateZ('+ hands[j].angle +'deg)';
+        elements[k].style.transform = 'rotateZ('+ hands[j].angle +'deg)';
+    }
+  }
+}
+
+    ////////////////  RESIZING
+
+    function jqUpdateSize(){
+
+    $('.site').flowtype({
+       minimum   : 100,
+       maximum   : 2000,
+       minFont   : 8,
+       maxFont   : 48.25,
+       fontRatio : 100
+    });
+};
+$(document).ready(jqUpdateSize);    // When the page first loads
+$(window).resize(jqUpdateSize);     // When the browser changes size
+
+
+    
+    
+    initKnife();
+  //end of function
+})();
